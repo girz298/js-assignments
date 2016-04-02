7b2609ed10b77719be5fd7943553446caa7962b4
sega298 <sega298@mail.ru>
2016-04-03 01:35:10 +0300

    date work

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   return (date.getFullYear()%4 === 0 && date.getFullYear()%100 !== 0)||(date.getFullYear()%400 === 0);
 }
 
 
@@ -76,7 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+
 }
 
 
@@ -94,7 +94,15 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    var hours = parseInt(date.getHours())===0?24:parseInt(date.getHours());
+    var minutes  = parseInt(date.getMinutes())===0?24:parseInt(date.getMinutes());
+    if(hours-minutes%2 === 0){
+        return Math.PI;
+    }else if(hours-minutes%3 === 0){
+        return Math.PI/2;
+    }else{
+        return 0;
+    }
 }
 
 
